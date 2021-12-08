3eefb1da6959e9b1d3c1b59f9523b055e2e7aa64
negaskolya <ShlykovNikolaj@yandex.ru>
2021-12-08 20:46:14 +0300

    added constants file

---
+++ game catch the ball/constants.py
@@ -0,0 +1,17 @@
+"""
+Константы, используемые в игре:
+цвета
+количество кадров в секунду
+размеры экрана
+"""
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+FPS = 30
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
